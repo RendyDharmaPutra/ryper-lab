f7eab7791c33f400cc78ed17cfc227a6b4dbda8d
RendyDharmaPutra <rendydharmaputra424@gmail.com>
2025-09-21 22:44:35 +0700

    feat: compose mobile nav section to individual component, implement motion react on MobileNavSection

--- src/components/layouts/header/HeaderSection.tsx
+++ src/components/layouts/header/HeaderSection.tsx
@@ -1,9 +1,9 @@
 import { DesktopNav } from "./navigation/DesktopNav";
 import { LogoHeader } from "./LogoHeader";
-import { MobileNav } from "./navigation/MobileNav";
 import { useState } from "react";
 import { MobileMenuBtn } from "./navigation/MobileMenuBtn";
 import { Menu, X } from "lucide-react";
+import { MobileNavSection } from "./navigation/MobileNavSection";
 
 export const HeaderSection = () => {
   const [show, setShow] = useState(false);
@@ -20,15 +20,7 @@ export const HeaderSection = () => {
         </div>
       </section>
 
-      {show && (
-        <section className="fixed top-0 right-0 px-4 py-7 h-screen w-64 bg-gray-900/95 backdrop-blur-3xl md:hidden z-[999]">
-          <div className="flex justify-end">
-            <MobileMenuBtn Icon={X} setShow={() => setShow(false)} />
-          </div>
-
-          <MobileNav />
-        </section>
-      )}
+      <MobileNavSection show={show} setShow={() => setShow(false)} />
     </header>
   );
 };
