88320aa8029df67548bb1da59e8fdf39682c3a48
RendyDharmaPutra <rendydharmaputra424@gmail.com>
2025-09-22 21:35:57 +0700

    feat(navigation): update nav components and rename blog page to blogs

--- src/components/layouts/header/navigation/DesktopNav.tsx
+++ src/components/layouts/header/navigation/DesktopNav.tsx
@@ -5,7 +5,7 @@ export const DesktopNav = () => {
   return (
     <nav className="hidden md:flex gap-10 space-x-6 lg:space-x-8">
       <NavItem Icon={House} nameRoute="Home" destinationRoute="/" />
-      <NavItem Icon={Globe} nameRoute="Blog" destinationRoute="/blog" />
+      <NavItem Icon={Globe} nameRoute="Blog" destinationRoute="/blogs" />
       <NavItem Icon={Users} nameRoute="About" destinationRoute="/about" />
     </nav>
   );
