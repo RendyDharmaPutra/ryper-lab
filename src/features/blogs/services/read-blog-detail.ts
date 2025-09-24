eb939d481df51fdd9278a0211dd688255505d578
RendyDharmaPutra <rendydharmaputra424@gmail.com>
2025-09-24 18:38:22 +0700

    feat: add type guard for checking return type of read-blog-detail-service

--- src/features/blogs/services/read-blog-detail.ts
+++ src/features/blogs/services/read-blog-detail.ts
@@ -2,6 +2,12 @@ import api from "@/lib/api";
 import type { BlogType } from "@/types/models/blog-type";
 import { handleApiError } from "@/utils/error/api-error-handler";
 
+export const isBlogType = (
+  data: BlogType | { error: string }
+): data is BlogType => {
+  return !("error" in data);
+};
+
 export const readBlogDetailService = async (
   id: string
 ): Promise<BlogType | { error: string }> => {
