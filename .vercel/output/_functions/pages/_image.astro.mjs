358bef80687fe8bbaf145cc2f85932e99d74a88f
lukydev <mochamadlukyadithiawork@gmail.com>
2025-10-02 16:19:38 +0700

    delete: static route for now

--- .vercel/output/_functions/pages/_image.astro.mjs
+++
@@ -1,2 +0,0 @@
-export { a as page } from '../chunks/generic_BwI0CEIj.mjs';
-export { renderers } from '../renderers.mjs';
--- .vercel/output/static/_astro/BackBtn.BhAxF1Gs.js
+++
@@ -1,6 +0,0 @@
-import{j as t}from"./jsx-runtime.D_zvdyIk.js";import{c as e}from"./createLucideIcon.DCnupLqN.js";import"./index.RH_Wq4ov.js";/**
- * @license lucide-react v0.544.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const o=[["path",{d:"m12 19-7-7 7-7",key:"1l729n"}],["path",{d:"M19 12H5",key:"x3x0zl"}]],r=e("arrow-left",o),n=()=>t.jsxs("button",{onClick:()=>history.back(),className:"back-button backdrop-blur-md cursor-pointer bg-white/10 px-6 py-3 rounded-2xl text-sm font-medium text-white hover:bg-white/20 flex items-center gap-2 shadow-lg",children:[t.jsx(r,{className:"w-4 h-4"}),"Kembali"]});export{n as BackBtn};
