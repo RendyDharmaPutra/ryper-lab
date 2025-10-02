ee449357823d41c16a267781b15643a7b71f084e
lukydev <mochamadlukyadithiawork@gmail.com>
2025-10-02 16:29:57 +0700

    delete: adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,11 @@ import { defineConfig } from "astro/config";
 import tailwind from "@tailwindcss/vite";
 import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
-import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://web-profile-ryper-astro.vercel.app",
   output: "static",
-  adapter: vercel({}),
   vite: {
     plugins: [tailwind()],
   },
