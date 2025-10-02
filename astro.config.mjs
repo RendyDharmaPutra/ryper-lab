744c1d0cadff7f93de2f8cee31179a95c0d3dfb0
lukydev <mochamadlukyadithiawork@gmail.com>
2025-10-02 16:41:38 +0700

    add: vercel.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,17 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
-import tailwind from "@tailwindcss/vite";
+import tailwindcss from "@tailwindcss/vite";
 import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
+import vercel from "@astrojs/vercel"; 
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://web-profile-ryper-astro.vercel.app",
-  output: "static",
+  output: "server",   // karena kamu pakai API
+  adapter: vercel(),  // aktifkan adapter vercel
   vite: {
-    plugins: [tailwind()],
+    plugins: [tailwindcss()],
   },
   integrations: [sitemap(), react()],
 });
